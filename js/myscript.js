4510a168e1402e7906b5532956ebe4a205625d08
Hong-Ming <h15731573@gmail.com>
2020-12-18 23:52:07 +0800

    update html

--- js/myscript.js
+++ js/myscript.js
@@ -10,3 +10,12 @@ window.onclick = function(e) {
     }
   }
 }
+
+jQuery(document).ready(function () {
+  var duration = 500;
+  jQuery('.back-to-top').click(function (event) {
+    event.preventDefault();
+    jQuery('html, body').animate({ scrollTop: 0 }, duration);
+    return false;
+  })
+});
